17495f2b7e3880e0abfe43a52674a311571d36d8
Luc Anchling <luc.anchling@cpe.fr>
2020-10-20 12:13:54 +0200

    Fin de séance du 20/10/2020

---
+++ JS/reservation.js
@@ -0,0 +1,22 @@
+
+function afficher(){
+
+    var nom =document.getElementById("name").value;
+    var prénom =document.getElementById("firstname").value;
+    var mail =document.getElementById("email").value;
+    var phone =document.getElementById("phone").value;
+    var gosse =document.getElementById("child").value;
+    var adulte =document.getElementById("adult").value;
+    var départ =document.getElementById("departure").value;
+    var retour =document.getElementById("return").value;
+    var dej =document.getElementById("name").value;
+    document.getElementById("texte2").value=nom ;
+    document.getElementById("texte3").value=prénom ;
+    document.getElementById("texte4").value=mail ;
+    document.getElementById("texte5").value=phone ;
+    document.getElementById("texte6").value=gosse ;
+    document.getElementById("texte7").value=adulte ;
+    document.getElementById("texte8").value=départ ;
+    document.getElementById("texte9").value=retour ;
+    document.getElementById("texte10").value=dej ;
+}
