4d23e358bff0440f37ed481d102b09e3a7291a9b
Luc Anchling <luc.anchling@cpe.fr>
2020-11-27 16:46:27 +0100

    Début du diaporama + fix récap

--- JS/Website.js
+++ JS/Website.js
@@ -1,3 +1,6 @@
+// Execution des fonctions
+window.onload = function() {scrollFunction()}
+
 // Création Tableau Destination
 var Destination = ["Canada","Espagne","Japon","Etats-Unis","Angleterre","Chine","Mexique","Italie"]
 
@@ -17,9 +20,24 @@ function scrollFunction() {
     }
 
 }
-
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
+// Diaporama des Destination :
+
+// Les Images
+var photos = new Array;
+$a = 0;//compteur de boucle
+$repertoire = opendir("../Images");//on ouvre le dossier 'images' qui contient toutes les images du diaporama
+while (($fichier = readdir())!=false)
+{
+        clearstatcache();
+        if($fichier!=".." && $fichier!=".")
+                {
+                echo "photos[$a] = 'images/$fichier';";//On place dans un tableau toutes les images trouvées dans le répertoire ouvert.
+                $a++;
+                }
+}
+closedir($repertoire); //on referme le dossier 'images'
